866777b52967c99f2dcf2ab38a867d92364c9c45
Meshwa Patel <125075708+meshwa-3027@users.noreply.github.com>
2024-06-17 19:17:52 +0530

    Add files via upload

---
+++ Historical_Data.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from alice_credentials import login 
+import pandas as pd
+
+
+alice = login() 
+
+instrument = alice.get_instrument_by_symbol("NSE", "NIFTY 50")
+from_datetime = datetime(2024,5,1,9,15,0)    # From last & days (yyyy,m,d,H,M,S) (date and timestamp)
+to_datetime = datetime(2024,5,31,15,30,0)    # To now
+interval = "1"       # ["1", "D"] 
+indices = True      # For Getting index data
+x = alice.get_historical(instrument, from_datetime, to_datetime, interval, indices)
+y = pd.DataFrame(x) 
+print(y)
+
+#########################################################################################################
+### Store data in Excel ###
+
+
+y.to_excel("historical_data.xlsx",index=False)
+print("Data Stored")
+
+
+########################################################################################################
+
+
+# Set the 'exchange_time_stamp' column as the index of the DataFrame
+y['exchange_time_stamp'] = pd.to_datetime(y['datetime'])
+
+y.set_index('exchange_time_stamp', inplace=True)
+
+# Resample the DataFrame for a 5-minute interval
+resampled_data = y.resample('15T').agg({
+    'open': 'first',
+    'high': 'max',
+    'low': 'min',
+    'close': 'last',
+    'volume': 'sum'
+})
+
+# Reset the index to make 'exchange_time_stamp' a regular column
+resampled_data.reset_index(inplace=True)
+
+print(resampled_data)
+
+resampled_data.to_excel("resampled_data.xlsx",index = False)
+print(" Resample Data stored")
